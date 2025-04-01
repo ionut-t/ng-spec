24450dad454664835531bc5da06dd0ac06d65204
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2025-04-01 19:50:51 +0100

    feat: add versioning information to CLI tool and improve release process

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,30 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
+
+// Version information - these will be set during the build process
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+const logo = "  \033[31m" + `
+   _        _______        _______  _______  _______  _______ 
+  ( (    /|(  ____ \      (  ____ \(  ____ )(  ____ \(  ____ \
+  |  \  ( || (    \/      | (    \/| (    )|| (    \/| (    \/
+  |   \ | || |      _____ | (_____ | (____)|| (__    | |      
+  | (\ \) || | ____(_____)(_____  )|  _____)|  __)   | |      
+  | | \   || | \_  )            ) || (      | (      | |      
+  | )  \  || (___) |      /\____) || )      | (____/\| (____/\
+  |/    )_)(_______)      \_______)|/       (_______/(_______/  
+  ` + "\033[0m"
 
 var rootCmd = &cobra.Command{
 	Use:   "ng-spec",
@@ -19,7 +40,10 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var component string
 
-		fmt.Println(component)
+		// Only print component if -v or --version not passed
+		if !cmd.Flags().Changed("version") {
+			fmt.Println(component)
+		}
 
 		if len(args) > 0 {
 			component = args[0]
@@ -27,6 +51,45 @@ var rootCmd = &cobra.Command{
 
 		generateComponentTest(component)
 	},
+	Version: version,
+}
+
+// init function to set version information from build info when available
+func init() {
+	// Try to get version info from Go module information
+	if info, ok := debug.ReadBuildInfo(); ok {
+		// Look for version in main module
+		if info.Main.Version != "(devel)" && info.Main.Version != "" {
+			version = strings.TrimPrefix(info.Main.Version, "v")
+		}
+
+		// Look for VCS info in settings
+		for _, setting := range info.Settings {
+			switch setting.Key {
+			case "vcs.revision":
+				if len(setting.Value) > 7 {
+					commit = setting.Value[:7]
+				} else if setting.Value != "" {
+					commit = setting.Value
+				}
+			case "vcs.time":
+				if t, err := time.Parse(time.RFC3339, setting.Value); err == nil {
+					date = t.Format("02/01/2006")
+				}
+			}
+		}
+	}
+}
+
+func init() {
+	versionTemplate := logo + `
+  Version                      %s
+  Commit                       %s
+  Release date	               %s
+`
+	versionTemplate = fmt.Sprintf(versionTemplate, version, commit, date)
+
+	rootCmd.SetVersionTemplate(versionTemplate)
 }
 
 func Execute() {
